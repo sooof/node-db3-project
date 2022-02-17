a7269b89a95dafdbb500b0b88d83179b2330b448
Mingjun Geng <sooofeve@gmail.com>
2022-02-16 20:05:16 -0800

    delete commit

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,22 +1,12 @@
 
 const db = require('../../data/db-config')
-const Scheme = require('./scheme-model')
 
-/*
-  If `scheme_id` does not exist in the database:
 
-  status 404
-  {
-    "message": "scheme with scheme_id <actual id> not found"
-  }
-*/
+
 const checkSchemeId = async (req, res, next) => {
-  // console.log(" middleware")
-  // next()
+
   try{
-    // const existScheme = await  Scheme.findById(req.params.scheme_id)
     const existScheme = await  db('schemes').where('scheme_id', req.params.scheme_id).first()
-    console.log(existScheme)
     if(!existScheme){
       next({ status: 404, message: `scheme with scheme_id ${req.params.scheme_id} not found` })
     }else{
@@ -28,17 +18,10 @@ const checkSchemeId = async (req, res, next) => {
   }
 }
 
-/*
-  If `scheme_name` is missing, empty string or not a string:
 
-  status 400
-  {
-    "message": "invalid scheme_name"
-  }
-*/
+
 const validateScheme = (req, res, next) => {
-  // console.log(" middleware")
-  // next()
+
   try{
     const {scheme_name} = req.body
     if( scheme_name === undefined  || typeof  scheme_name !=='string' || !scheme_name.trim() ){
@@ -51,18 +34,9 @@ const validateScheme = (req, res, next) => {
   }
 }
 
-/*
-  If `instructions` is missing, empty string or not a string, or
-  if `step_number` is not a number or is smaller than one:
 
-  status 400
-  {
-    "message": "invalid step"
-  }
-*/
 const validateStep = (req, res, next) => {
-  // console.log(" middleware")
-  // next()
+
   try{
     const {instructions, step_number} =req.body
     if(!instructions  || typeof  instructions !=='string' || !instructions.trim() 
